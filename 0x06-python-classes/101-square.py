acac2512795f9b877539956593e098fb1f3f61b6
Barackhalogen <barackhalogen@gmail.com>
2022-12-21 17:12:35 -0800

    Task 8. Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 
 
@@ -8,6 +7,7 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
+        
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
@@ -38,6 +38,7 @@ class Square:
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
