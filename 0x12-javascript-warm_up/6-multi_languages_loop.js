e8e3dd1deef4d6a660e4c16a5a8b7d20da1bef1a
Barackhalogen <barackhalogen@gmail.com>
2023-03-14 06:30:30 -0700

    Update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-/*
-   A JS Script that prints the elements of an array in a single line.
-   Author: Barackhalogen
-   Using for loop to get this done, however other loop can still work.
-*/
 let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0, len = myArray.length; i < len; i++) {
